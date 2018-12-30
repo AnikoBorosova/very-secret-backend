196a67d817ff22e15cd2a4c27b650efbee93f720
Anikó Borosová <borosanikoo@gmail.com>
2018-12-30 16:18:16 +0100

    CORS issue in progress

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
+//var cors = require('cors');
 
 const PORT = process.env.PORT || 4001;
 
-app.get("/expenses", (req, res, next) => {
+//app.use(cors({origin: 'http://localhost:8080'}));
+
+app.post("/expenses", (req, res) => {
+	console.log("SSSSSSSSSSSSSSSSSSSSSS")
+
+	const requestIn = req;
+	console.log("REQ", requestIn);
+
 	const reqBody = req.body;
 	console.log("REQUEST BODY", reqBody);
-	res.status(200).send(console.log("Communication between client-side and server is working"));
+
+	//var origin = req.headers.origin;
+
+	res.status(200)
+		.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/")
+		//.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+		//.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+		.setHeader('Access-Control-Allow-Credentials', true)
+		.send(console.log("Communication between client-side and server is working"));
 });
 
 app.listen(PORT, () => {
