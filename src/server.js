89208fca90ad15999caeb826ca04c3f596ce22f1
Anikó Borosová <borosanikoo@gmail.com>
2019-02-10 15:57:41 +0100

    cors in progress

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 //var cors = require('cors');
 
@@ -6,25 +8,36 @@ const PORT = process.env.PORT || 4001;
 
 //app.use(cors({origin: 'http://localhost:8080'}));
 
-app.post("/expenses", (req, res) => {
-	console.log("SSSSSSSSSSSSSSSSSSSSSS")
+app.use(bodyParser.json({
+	limit: "10mb"
+}));
+app.use(cookieParser("asd"));
 
+app.post("/expenses", (req, res) => {
 	const requestIn = req;
-	console.log("REQ", requestIn);
+	// console.log("REQ", requestIn);
 
 	const reqBody = req.body;
 	console.log("REQUEST BODY", reqBody);
 
 	//var origin = req.headers.origin;
 
-	res.status(200)
-		.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/")
-		//.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-		//.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-		.setHeader('Access-Control-Allow-Credentials', true)
-		.send(console.log("Communication between client-side and server is working"));
+	/* res.status(200) 
+    .setHeader("Access-Control-Allow-Origin", "http://localhost:8080/") 
+    //.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') 
+    //.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type') 
+    .setHeader('Access-Control-Allow-Credentials', true) 
+    .send(console.log("Communication between client-side and server is working"));  */
+
+	res.json({
+		result: "ASD"
+	})
 });
 
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, err => {
+	if (err) {
+		return console.error("Error while starting server:", err);
+	}
+
+	console.log(`Server is listening on PORT ${PORT}`);
 });
