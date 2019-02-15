0f36ba7d28adee464794b5e32a6da0b433fa401f
Anikó Borosová <borosanikoo@gmail.com>
2019-02-15 12:59:35 +0100

    cors in -  works somehow without WithCredentials

--- src/server.js
+++ src/server.js
@@ -1,36 +1,49 @@
-const express = require("express");
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const app = express();
-//var cors = require('cors');
+
+const cors = require('cors');
+const express = require("express");
+let app = express();
+
+app.use(cors());
+app.options('*', cors());
 
 const PORT = process.env.PORT || 4001;
 
 //app.use(cors({origin: 'http://localhost:8080'}));
 
-app.use(bodyParser.json({
-	limit: "10mb"
+/*app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});*/
+
+app.use(bodyParser.json({ 
+  limit: "10mb" 
 }));
-app.use(cookieParser("asd"));
+
+
+//app.use("/expenses", cors({origin: 'http://localhost:8080'}));
 
 app.post("/expenses", (req, res) => {
 	const requestIn = req;
-	// console.log("REQ", requestIn);
+	console.log("REQ", requestIn);
 
 	const reqBody = req.body;
 	console.log("REQUEST BODY", reqBody);
 
 	//var origin = req.headers.origin;
 
-	/* res.status(200) 
-    .setHeader("Access-Control-Allow-Origin", "http://localhost:8080/") 
-    //.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') 
-    //.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type') 
-    .setHeader('Access-Control-Allow-Credentials', true) 
-    .send(console.log("Communication between client-side and server is working"));  */
-
-	res.json({
-		result: "ASD"
+	res.status(200).json({
+		result: "Communication between client-side and server is working"
 	})
 });
 
@@ -39,5 +52,5 @@ app.listen(PORT, err => {
 		return console.error("Error while starting server:", err);
 	}
 
-	console.log(`Server is listening on PORT ${PORT}`);
+	console.log(`Server is listening on PORT ${PORT}`, app);
 });
